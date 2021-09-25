675f1d5e59436b8dc962baf9c010ae0bc2b59867
Vlad Zusko <vladzvu11@gmail.com>
2021-09-25 16:09:21 +0300

    Fixed burger on mobiles

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,20 @@
-let teamSwiper = new Swiper('.team-members', {
+const Breakpoints = {
+    MD: 768,
+    LG: 1440,
+};
+
+const teamSwiper = new Swiper('.team-members', {
     spaceBetween: 40,
     breakpoints: {
         0: {
             slidesPerGroup: 1,
             slidesPerView: 1,
         },
-        768: {
+        [Breakpoints.MD]: {
             slidesPerGroup: 2,
             slidesPerView: 2,
         },
-        1440: {
+        [Breakpoints.LG]: {
             slidesPerGroup: 4,
             slidesPerView: 4,
         },
@@ -21,3 +26,33 @@ let teamSwiper = new Swiper('.team-members', {
     observer: true,
     observeParents: true,
 });
+
+const navigationOpenEl = document.querySelector('.navigation__open');
+const navigationCloseEl = document.querySelector('.navigation__close');
+
+function toggleNavigation() {
+    const navigationEl = document.querySelector('.navigation');
+
+    if (window.innerWidth <= Breakpoints.MD) {
+        navigationEl.classList.add('navigation--burger');
+    } else {
+        navigationEl.classList.remove('navigation--burger');
+        navigationEl.classList.remove('navigation--burger-active');
+    }
+}
+
+function openNavigation() {
+    const navigationEl = document.querySelector('.navigation');
+    navigationEl.classList.add('navigation--burger-active');
+}
+
+function closeNavigation() {
+    const navigationEl = document.querySelector('.navigation');
+    navigationEl.classList.remove('navigation--burger-active');
+}
+
+navigationOpenEl.addEventListener('click', openNavigation);
+navigationCloseEl.addEventListener('click', closeNavigation);
+
+window.addEventListener('load', toggleNavigation);
+window.addEventListener('resize', toggleNavigation);
